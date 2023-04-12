3db2df140bc38fe64e739d6e38be0423fc5bec4d
Afo <obaone01@gmail.com>
2023-04-12 01:48:06 -0700

    removed use theme and did some navbar updates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,39 @@
-import { useEffect } from "react";
+import shakespeareHead from "/vite.svg";
 
-import viteLogo from "/vite.svg";
+import { useTheme } from "./hooks/useTheme";
+import { useSearch } from "./hooks/useSearch";
 
 import { Chips } from "./components/Chips";
 import { Search } from "./components/Search";
 import { Results } from "./components/Results";
-import { useSearch } from "./hooks/useSearch";
-import { themeChange } from "theme-change";
 
 function App() {
+  const { toggleTheme } = useTheme();
   const { searchRef, state, handleSearchChange, handleSetQuery } = useSearch();
-
-  useEffect(() => {
-    themeChange(false);
-  }, []);
 
   return (
     <>
-      <header className="">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <div>
-          <button
-            data-toggle-theme="dark,light"
-            data-act-class="ACTIVECLASS"
-            className="btn"
+      <header className="navbar bg-primary">
+        <div className="navbar-start flex gap-1">
+          <a
+            href="https://www.shakespeare.org.uk/explore-shakespeare/shakespedia/william-shakespeare/william-shakespeare-biography"
+            target="_blank"
           >
-            Toggle
+            <img
+              src={shakespeareHead}
+              className="logo"
+              alt="Shaking Shakespeare"
+            />
+          </a>
+          <h1 className="">Search</h1>
+        </div>
+        <div className="navbar-end">
+          <button onClick={toggleTheme} className="btn">
+            Theme
           </button>
         </div>
       </header>
-      <main className="flex flex-col gap-2">
+      <main className="flex flex-col gap-4 px-4 py-6">
         <Search
           ref={searchRef}
           onSearchChange={handleSearchChange}
