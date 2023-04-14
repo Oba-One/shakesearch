66a13e337a819713bffd41f329a7b270f780985e
Afo <obaone01@gmail.com>
2023-04-13 20:43:13 -0700

    fixed initial theme setting

--- client/src/hooks/useTheme.ts
+++ client/src/hooks/useTheme.ts
@@ -4,7 +4,7 @@ type Theme = "light" | "dark";
 
 export const useTheme = () => {
   const [theme, setTheme] = useState<Theme>(
-    localStorage.getItem("theme") as Theme
+    (localStorage.getItem("theme") as Theme) ?? "light"
   );
 
   const toggleTheme = () => {
